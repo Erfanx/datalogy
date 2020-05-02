ca8543213d870b32397be5b1349f51a0c8205808
Erfanx <irfansyed479@gmail.com>
2020-05-02 08:03:20 +0600

    Refactor testing file

--- Testing.R
+++ Testing.R
@@ -3,3 +3,24 @@ print("Hello World!")
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+
+# Q1
+set.seed(1)
+rpois(5, 2)
+
+
+# Q5
+
+set.seed(10)
+x <- rep(0:1, each = 5)
+e <- rnorm(10, 0, 20)
+y <- 0.5 + 2 * x + e
+y
+
+# Q8
+library(datasets)
+Rprof()
+fit <- lm(y ~ x1 + x2)
+Rprof(NULL)
+
