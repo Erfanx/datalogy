d9c57609f40273d47437647fea6c1651ce27bcca
Erfanx <irfansyed479@gmail.com>
2020-05-16 23:56:06 +0600

    Basic R Programming

--- R Basic/function.r
+++ R Basic/function.r
@@ -13,11 +13,11 @@ above <- function(x, n){
   x[use]
 }
 
-columnmean <- function(y){
+columnmean <- function(y, removeNA = TRUE){
   nc <- ncol(y)
   means <- numeric(nc)
   for(i in 1:nc){
-    means[i] <- mean(y[, i])
+    means[i] <- mean(y[, i], na.rm = removeNA)
   }
   means
 }
